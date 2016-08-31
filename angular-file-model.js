ad2e04e4678a82ff149a9114efa0c1b626609608
Deepak Ramesh <ncdram@gmail.com>
2016-08-31 07:25:58 +0530

    Module works when using grunt serve but has issues when doing a build. Removed the anonymous function for directive which fixes the problem. Need to review the approach.

--- angular-file-model.js
+++ angular-file-model.js
@@ -5,26 +5,21 @@
 // Directive that makes the inputs with type `file` to be
 // available in the `$scope` and be assigned to a model.
 //
-(function () {
-  'use strict';
-
-  angular.module('file-model', [])
-
+angular.module('file-model', [])
   .directive('fileModel', [
     '$parse',
-    function ($parse) {
+    function($parse) {
       return {
         restrict: 'A',
         link: function(scope, element, attrs) {
           var model = $parse(attrs.fileModel);
           var modelSetter = model.assign;
 
-          element.bind('change', function(){
-            scope.$apply(function(){
+          element.bind('change', function() {
+            scope.$apply(function() {
               if (attrs.multiple) {
                 modelSetter(scope, element[0].files);
-              }
-              else {
+              } else {
                 modelSetter(scope, element[0].files[0]);
               }
             });
@@ -33,5 +28,3 @@
       };
     }
   ]);
-
-})();
